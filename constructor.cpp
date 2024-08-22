4ed0043ac5261dbc6e06cf1e58112412c8e68951
gaurav7825 <1.gauravkandpal@gmail.com>
2024-08-22 18:50:04 +0530

    Constructor_program

---
+++ constructor.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class student{
+public:
+int id;
+string name;
+string course;
+
+void stu(){
+    course="B tech cse";
+}
+};
+
+int main()
+{
+    student stu1;
+
+    stu1.id=1111;
+    stu1.name="Gaurav";
+    stu1.id;
+
+
+    return 0;
+}
